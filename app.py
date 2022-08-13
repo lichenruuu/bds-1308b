a71884d1620edb89cf16271ec4ac47ab26ba4827
lichenruuu <111170272+lichenruuu@users.noreply.github.com>
2022-08-13 05:04:23 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install flask
+
+
+# In[2]:
+
+
+from flask import Flask,render_template,request
+
+
+# In[3]:
+
+
+import joblib
+
+
+# In[4]:
+
+
+app = Flask(__name__)
+
+
+# In[5]:
+
+
+@app.route("/",methods=["GET","FOST"])
+def index():
+    if request.method == "POST":
+        rates = float(request.form.get("rates"))
+        print(rates)
+        model1 = joblib.load("regression_DBS")
+        pred1 = model1.predict([[rates]])
+        model2 = joblib.load("tree_DBS")
+        pred2 = model2.predict([[rates]])
+        return(render_template("index.html",result="temp",result2="temp"))
+    else:
+        return(render_template("index.html",result1="waiting",result2="waiting"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run(host='local_host', port=5000, debug=False)
+
+
+# In[ ]:
+
+
+__name__
+
+
+# In[ ]:
+
+
+
+
