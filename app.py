5bc65beb8a2c3a8acad775c3473644723f431df8
lichenruuu <111170272+lichenruuu@users.noreply.github.com>
2022-08-13 11:01:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,43 @@
-#!/usr/bin/env python
-# coding: utf-8
 
 # In[1]:
 
 
+from flask import Flask, render_template, request
 
 
 # In[2]:
 
 
-from flask import Flask,render_template,request
+import joblib
 
 
 # In[3]:
 
 
-import joblib
+app = Flask(__name__)
 
 
 # In[4]:
 
 
-app = Flask(__name__)
-
-
-# In[5]:
-
-
-@app.route("/",methods=["GET","FOST"])
+@app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        rates = float(request.form.get("rates"))
+        rates=float(request.form.get("rates"))
         print(rates)
-        model1 = joblib.load("regression_DBS")
-        pred1 = model1.predict([[rates]])
-        model2 = joblib.load("tree_DBS")
-        pred2 = model2.predict([[rates]])
-        return(render_template("index.html",result=pred1,result2=pred2))
+        model1=joblib.load("regression_DBS")
+        pred1=model1.predict([[rates]])
+        model2=joblib.load("tree_DBS")
+        pred2=model2.predict([[rates]])
+        return(render_template("index.html",result1=pred1,result2=pred2))
     else:
-        return(render_template("index.html",result1="waiting",result2="waiting"))
+        return(render_template("index.html",result1="waiting",result2="waiting"))  
 
 
 # In[ ]:
 
 
 if __name__ == "__main__":
+    
     app.run()
-
-
-# In[ ]:
-
-
-__name__
-
-
-# In[ ]:
-
-
-
 
